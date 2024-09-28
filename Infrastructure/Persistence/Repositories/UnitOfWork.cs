41663e41865147e2bd9d9d9dd26392f298d43678
mohamedFathy98 <mdx7700@gmail.com>
2024-09-29 00:26:22 +0300

    Vid 10 => UnitOfWork

---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Persistence.Data;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreContext _storeContext;
+        private readonly ConcurrentDictionary<string, Object> _repositories;
+        public UnitOfWork(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+            _repositories = new();
+        }
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        =>
+          (IGenericRepository<TEntity, TKey>)_repositories.GetOrAdd(typeof(TEntity).Name, _ => new GenericRepository<TEntity, TKey>(_storeContext));
+
+          //var typeName=typeof(TEntity).Name;
+          // if(_repositories.ContainsKey(typeName))return(IGenericRepository<TEntity,TKey>) _repositories[typeName];
+
+          // var repo = new GenericRepository<TEntity, TKey>(_storeContext);
+          // _repositories.Add(typeName, repo);
+          // return repo;
+          // return new GenericRepository<TEntity, TKey>(_storeContext);
+
+
+
+        public async Task<int> SaveChangesAsync() => await _storeContext.SaveChangesAsync();
+
+    }
+}
