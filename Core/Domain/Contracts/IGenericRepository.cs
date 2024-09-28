cd6c3ee85eff9692b4d31b7a54a30d3fe14c3632
mohamedFathy98 <mdx7700@gmail.com>
2024-09-29 00:17:46 +0300

    Vid 09 => Generic Rep

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        Task<TEntity?> GetAsync(TKey id);
+        Task<IEnumerable<TEntity>> GetAllAsync(bool trackChange);
+
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
