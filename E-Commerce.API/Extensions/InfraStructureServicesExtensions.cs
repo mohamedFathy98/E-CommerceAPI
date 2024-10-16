b81a5c09f7440a1b69bdf720ccd1beef5849644d
mohamedFathy98 <mdx7700@gmail.com>
2024-10-16 05:49:16 +0300

    Vid02

--- E-Commerce.API/Extensions/InfraStructureServicesExtensions.cs
+++ E-Commerce.API/Extensions/InfraStructureServicesExtensions.cs
@@ -5,6 +5,8 @@ using Persistence.Repositories;
 using Persistence;
 using StackExchange.Redis;
 using Persistence.Identity;
+using Domain.Entites;
+using Microsoft.AspNetCore.Identity;
 
 namespace E_Commerce.API.Extensions
 {
@@ -27,6 +29,21 @@ namespace E_Commerce.API.Extensions
             });
             services.AddSingleton<IConnectionMultiplexer>(_ => ConnectionMultiplexer.Connect
             (configuration.GetConnectionString("Redis")!));
+            services.ConfigureIdentityServices();
+            return services;
+        }
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services)
+        {
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 8;
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<StoreIdentityContext>();
             return services;
         }
     }
