1d340fbd6a00f584d6d87c8cde11c5a5dc114ff3
mohamedFathy98 <mdx7700@gmail.com>
2024-10-21 17:08:16 +0300

    Vid 04

---
+++ Core/Services/MappingProfiles/OrderProfile.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entites.OrderEntities;
+using Shared.OrderModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.MappingProfiles
+{
+    internal class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Address, AddressDTO>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(d => d.ProductId,
+                options => options.MapFrom
+                (s => s.Product.ProductId))
+            .ForMember(d => d.ProductName,
+                options => options.MapFrom
+                (s => s.Product.ProductName))
+           .ForMember(d => d.PicturetUrl,
+                options => options.MapFrom
+                (s => s.Product.PictureUrl)); ;
+
+
+
+            CreateMap<Order, OrderResult>()
+                .ForMember(d => d.PaymentStatus,
+                options => options.MapFrom
+                (s => s.ToString()))
+                .ForMember(d => d.DeliveryMethod,
+               options => options.MapFrom
+               (s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.Total,
+               options => options.MapFrom
+               (s => s.SubTotal + s.DeliveryMethod.Price));
+
+            CreateMap<DeliveryMethod, DeliveryMethodResult>();
+        }
+    }
+}
